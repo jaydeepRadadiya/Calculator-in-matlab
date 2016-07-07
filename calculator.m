a10b0bc4ce9ce32b8a176e450b149aec2c4b9209
jaydeepRadadiya <jaydeep.radadiya.9041@gmail.com>
2016-07-07 11:54:58 +0530

    Add files via upload

---
+++ calculator.m
@@ -0,0 +1,360 @@
+function varargout = untitled(varargin)
+% UNTITLED MATLAB code for untitled.fig
+%      UNTITLED, by itself, creates a new UNTITLED or raises the existing
+%      singleton*.
+%
+%      H = UNTITLED returns the handle to a new UNTITLED or the handle to
+%      the existing singleton*.
+%
+%      UNTITLED('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in UNTITLED.M with the given input arguments.
+%
+%      UNTITLED('Property','Value',...) creates a new UNTITLED or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before untitled_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to untitled_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help untitled
+
+% Last Modified by GUIDE v2.5 04-Feb-2016 22:58:52
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @untitled_OpeningFcn, ...
+                   'gui_OutputFcn',  @untitled_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before untitled is made visible.
+function untitled_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to untitled (see VARARGIN)
+
+% Choose default command line output for untitled
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes untitled wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = untitled_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in add.
+function add_Callback(hObject, eventdata, handles)
+% hObject    handle to add (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+textString=get(handles.z,'string');
+textString=strcat(textString,'+');
+set(handles.z,'string',textString);
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function y_Callback(hObject, eventdata, handles)
+% hObject    handle to y (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of y as text
+%        str2double(get(hObject,'String')) returns contents of y as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function y_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to y (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function x_Callback(hObject, eventdata, handles)
+% hObject    handle to x (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of x as text
+%        str2double(get(hObject,'String')) returns contents of x as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function x_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to x (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in sub.
+function sub_Callback(hObject, eventdata, handles)
+% hObject    handle to sub (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+textString=get(handles.z,'string');
+textString=strcat(textString,'-');
+set(handles.z,'string',textString);
+
+% --- Executes on button press in mul.
+function mul_Callback(hObject, eventdata, handles)
+% hObject    handle to mul (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+textString=get(handles.z,'string');
+textString=strcat(textString,'*');
+set(handles.z,'string',textString);
+
+% --- Executes on button press in div.
+function div_Callback(hObject, eventdata, handles)
+% hObject    handle to div (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+textString=get(handles.z,'string');
+textString=strcat(textString,'/');
+set(handles.z,'string',textString);
+
+
+% --- Executes on button press in two.
+function two_Callback(hObject, eventdata, handles)
+% hObject    handle to two (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+textString=get(handles.z,'string');
+if(strcmp(textString,'0')==1)
+    
+        set(handles.z,'string','2');
+else
+    textString=strcat(textString,'2');
+    set(handles.z,'string',textString);
+end
+
+% --- Executes on button press in three.
+function three_Callback(hObject, eventdata, handles)
+% hObject    handle to three (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+textString=get(handles.z,'string');
+if(strcmp(textString,'0')==1)
+    
+        set(handles.z,'string','3');
+else
+    textString=strcat(textString,'3');
+    set(handles.z,'string',textString);
+end
+
+% --- Executes on button press in four.
+function four_Callback(hObject, eventdata, handles)
+% hObject    handle to four (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+textString=get(handles.z,'string');
+if(strcmp(textString,'0')==1)
+    
+        set(handles.z,'string','4');
+else
+    textString=strcat(textString,'4');
+    set(handles.z,'string',textString);
+end
+
+% --- Executes on button press in six.
+function six_Callback(hObject, eventdata, handles)
+% hObject    handle to six (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+textString=get(handles.z,'string');
+if(strcmp(textString,'0')==1)
+    
+        set(handles.z,'string','6');
+else
+    textString=strcat(textString,'6');
+    set(handles.z,'string',textString);
+end
+% --- Executes on button press in seven.
+function seven_Callback(hObject, eventdata, handles)
+% hObject    handle to seven (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+textString=get(handles.z,'string');
+if(strcmp(textString,'0')==1)
+    
+        set(handles.z,'string','7');
+else
+    textString=strcat(textString,'7');
+    set(handles.z,'string',textString);
+end
+
+% --- Executes on button press in eight.
+function eight_Callback(hObject, eventdata, handles)
+% hObject    handle to eight (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+textString=get(handles.z,'string');
+if(strcmp(textString,'0')==1)
+    
+        set(handles.z,'string','8');
+else
+    textString=strcat(textString,'8');
+    set(handles.z,'string',textString);
+end
+
+% --- Executes on button press in zero.
+function zero_Callback(hObject, eventdata, handles)
+% hObject    handle to zero (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+textString=get(handles.z,'string');
+if(strcmp(textString,'0')==1)
+    
+        set(handles.z,'string','0');
+else
+    textString=strcat(textString,'0');
+    set(handles.z,'string',textString);
+end
+
+% --- Executes on button press in neven.
+function neven_Callback(hObject, eventdata, handles)
+% hObject    handle to neven (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+textString=get(handles.z,'string');
+if(strcmp(textString,'0')==1)
+    
+        set(handles.z,'string','9');
+else
+    textString=strcat(textString,'9');
+    set(handles.z,'string',textString);
+end
+
+% --- Executes on button press in one.
+function one_Callback(hObject, eventdata, handles)
+% hObject    handle to one (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+textString=get(handles.z,'string');
+if(strcmp(textString,'0')==1)
+    
+        set(handles.z,'string','1');
+else
+    textString=strcat(textString,'1');
+    set(handles.z,'string',textString);
+end
+        
+    
+
+% --- Executes on button press in five.
+function five_Callback(hObject, eventdata, handles)
+% hObject    handle to five (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+textString=get(handles.z,'string');
+if(strcmp(textString,'0')==1)
+    
+        set(handles.z,'string','5');
+else
+    textString=strcat(textString,'5');
+    set(handles.z,'string',textString);
+end
+
+% --- Executes during object creation, after setting all properties.
+function z_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to z (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+
+% --- Executes on button press in equ.
+function equ_Callback(hObject, eventdata, handles)
+% hObject    handle to equ (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+textString=get(handles.z,'string');
+answer =eval(textString);
+set(handles.z,'String',answer)
+
+% --- Executes on button press in c.
+function c_Callback(hObject, eventdata, handles)
+% hObject    handle to c (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+set(handles.z,'String','0')
+
+
+% --- Executes on button press in dot.
+function dot_Callback(hObject, eventdata, handles)
+% hObject    handle to dot (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+textString=get(handles.z,'string');
+if(strcmp(textString,'0')==1)
+    
+        set(handles.z,'string','.');
+else
+    textString=strcat(textString,'.');
+    set(handles.z,'string',textString);
+end
